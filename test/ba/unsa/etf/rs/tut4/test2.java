cd631c2735c3db2594f0d19868a6ccb53f6bd71b
yahia aissa el hattab <eyahiaaiss1@etf.unsa.ba>
2020-04-03 23:57:36 +0200

    drugi test

--- test/ba/unsa/etf/rs/tut4/test2.java
+++ test/ba/unsa/etf/rs/tut4/test2.java
@@ -23,6 +23,29 @@ public class test2 {
         stage.setScene(new Scene(root));
         stage.show();
     }
+    @Test
+    public void test2_1(FxRobot robot) {
+        robot.lookup("#dodaj").queryButton();
+        robot.lookup("#ulaz").queryTextInputControl();
+        robot.lookup("#izlaz").queryTextInputControl(); }
+    @Test
+    public void test2_2(FxRobot robot) {
+        robot.clickOn("#ulaz");
+        robot.write("mlj1,mljeko1,2");
+        robot.clickOn("#dodaj");
+        TextArea textArea = (TextArea)robot.lookup("#izlaz").query();
+        assertEquals("mlj1,mljeko1,2.0\n", textArea.getText());
+    }
+
+    @Test
+    public void test2_3(FxRobot robot) {
+        robot.clickOn("#ulaz");
+        robot.write("mlj1,mljeko1,2\n"+ "mlj2,mljeko2,1.3\n" + "mlj3,mljeko3,2.25");
+        robot.clickOn("#dodaj");
+        TextArea textArea = (TextArea)robot.lookup("#izlaz").query();
+        assertEquals("mlj1,mljeko1,2.0\n" + "mlj2,mljeko2,1.3\n" + "mlj3,mljeko3,2.25\n", textArea.getText());
+    }
+
 
 }
 
